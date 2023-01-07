27e88709ae37bbe587909f320ead734cbe3091f3
Byron Lloyd-Wakeman <byzstorm1103@gmail.com>
2023-01-07 14:37:32 +0000

    Created entry page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Component } from 'react';
+import StatisticsBar from './Components/statisticsbar.jsx';
+import TargetInput from './Components/targetinput.jsx';
+import UserInput from './Components/userinput.jsx';
+import RetryButton from './Components/retrybutton.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import DropDowns from './Components/dropdowns.jsx';
+
+class App extends Component {
+  render(){
+    return(
+      <div className="container">
+        <div className="title__container">
+          <h1 className="title">Typing test</h1>
+        </div>
+        <DropDowns/>
+        <StatisticsBar Speed="40" Accuracy="100" AdjustedSpeed="40"/>
+        <TargetInput/>
+        <UserInput/>
+        <RetryButton/>
+      </div>
+    )
+  }
 }
 
 export default App;
