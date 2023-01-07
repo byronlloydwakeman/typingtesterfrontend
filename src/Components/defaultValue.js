02028bfeadd9d19ed87b31e899f943da212d5c5b
Byron Lloyd-Wakeman <byzstorm1103@gmail.com>
2023-01-07 14:38:04 +0000

    Changed entry point format

---
+++ src/Components/defaultValue.js
@@ -0,0 +1,5 @@
+const Data = {
+    "defaultValue": "The Wind in the Willows is a children's novel by the British novelist Kenneth Grahame, first published in 1908. It details the story of Mole, Ratty, and Badger as they try to help Mr. Toad, after he becomes obsessed with motorcars and gets into trouble. It also details short stories about them that are disconnected from the main narrative. The novel was based on bedtime stories Grahame told his son Alastair. It has been adapted numerous times for both stage and screen. The Wind in the Willows received negative reviews upon its initial release, but has since become a classic of British literature. It was listed at No. 16 in the BBC's survey The Big Read, and has been adapted multiple times in different mediums"
+}
+
+export default Data;
